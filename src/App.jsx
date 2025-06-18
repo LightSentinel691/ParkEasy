587c7822b9b7be91ba041cf56391048cf6af781d
Timothy Kiige <Timothykiige98@gmail.com>
2025-06-18 14:04:11 +0300

    feat: Implement authentication state management and protect BookingConfirmation route

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import AuthenticationHome from "./Components/Authentication/AuthenticationHome";
 import LoginPage from "./Components/Authentication/LoginPage";
 import Homepage from "./Components/Homepage";
 import RegisterPage from "./Components/Authentication/Registration";
+import BookingConfirmation from "./Components/BookingConfirmation";
+import { useState } from "react";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/Authentication" element={<AuthenticationHome />}>
+          <Route path="/Authentication" element={<AuthenticationHome setIsAuthenticated={setIsAuthenticated}/>}>
             <Route index element={<LoginPage />} />
             <Route path="Register" element={<RegisterPage />} />
           </Route>
+          <Route
+            path="/confirm"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <BookingConfirmation />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
