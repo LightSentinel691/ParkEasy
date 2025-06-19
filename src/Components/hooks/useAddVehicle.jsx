4622ee111d0a713a07c38d9c34f4c3b092dcc7e2
Timothy Kiige <Timothykiige98@gmail.com>
2025-06-19 16:55:56 +0300

    feat: Implement useAddVehicle hook for adding vehicle data to Firestore

---
+++ src/Components/hooks/useAddVehicle.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db, auth } from "../../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const useAddVehicle = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const addVehicle = async ({ plate, ownerName, phone, email, timeIn }) => {
+    setLoading(true);
+    setError(null);
+
+    const now = new Date();
+    // Format time String
+    const timeString = now.toLocaleTimeString();
+
+    // Format the date string
+    const dateString = now.toLocaleDateString();
+
+    const dateTimeString = `${dateString} ${timeString}`;
+
+    return new Promise((resolve, reject) => {
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          try {
+            const docRef = await addDoc(collection(db, "vehicles"), {
+              vehiclePlate: plate,
+              ownerName,
+              ownerPhone: phone,
+              ownerEmail: email,
+              timeIn,
+              clockIn: dateTimeString,
+              status: "Ongoing",
+              slot: "spot-1",
+              createdBy: user.uid,
+              createdAt: serverTimestamp(),
+            });
+            resolve(docRef);
+          } catch (err) {
+            setError(err.message);
+            reject(err);
+          } finally {
+            setLoading(false);
+          }
+        } else {
+          setError("User not logged in");
+          setLoading(false);
+          reject("User not logged in");
+        }
+      });
+    });
+  };
+
+  return { addVehicle, loading, error };
+};
+
+export default useAddVehicle;
+
+//Remove the hardcoded Slot value to get the value dynamically
